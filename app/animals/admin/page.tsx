34c23420b25ebf74173b49044cf799f91e7960f9
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-05-31 18:40:16 +0200

    Add mock authorization

--- app/animals/admin/page.tsx
+++ app/animals/admin/page.tsx
@@ -1,6 +1,32 @@
+import { gql } from '@apollo/client';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { getClient } from '../../../util/client';
 import AdminDashboard from './AdminDashboard';
 
-export default function AnimalsAdminPage() {
+export default async function AnimalsAdminPage() {
+  const fakeSessionToken = cookies().get('fakeSession');
+
+  const { data } = await getClient().query({
+    query: gql`
+      query LoggedInAnimal($firstName: String!) {
+        loggedInAnimalByFirstName(firstName: $firstName) {
+          id
+          firstName
+          type
+          accessory
+        }
+      }
+    `,
+    variables: {
+      firstName: fakeSessionToken?.value || '',
+    },
+  });
+
+  if (!data.loggedInAnimalByFirstName) {
+    redirect('/login');
+  }
+
   return (
     <div>
       <AdminDashboard />
